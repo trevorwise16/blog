8fc3dfed5c35e2b8284535a7e6f2ed42ff24f5a8
trevorwise16 <trevorwise16@berkeley.edu>
2025-07-11 15:54:00 -0700

    linted

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,4 @@ import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from '@vercel/postgres';
 import * as schema from './schema';
 
-// Only load dotenv in development
-if (process.env.NODE_ENV === 'development') {
-  const { config } = require('dotenv');
-  config({ path: '.env' });
-}
-
 export const db = drizzle(sql, { schema });
